43d34e564035738d66da715b9478a7acc5c3f7dd
zetgtr <artem.kudrin70@gmail.com>
2022-05-26 23:52:04 +0300

    Update v_main.php
    Папку поменял и забыл

--- lesson-5/v/v_main.php
+++ lesson-5/v/v_main.php
@@ -7,9 +7,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
     </script>
-    <script src="js/jquery.js"></script>
+    <script src="./data/js/jquery.js"></script>
     <script src="https://malsup.github.io/jquery.form.js"></script>
-    <script src="/data/js/script.js"></script>
+    <script src="./data/js/script.js"></script>
     <title><?=$title ?></title>
     <link rel="stylesheet" type="text/css" href="style.css" />
 </head>

01d85a2809d234e1fed0d8b2a86ea989a40e1d6b
Artem <artem.kudrin70@gmail.com>
2022-05-27 20:23:14 +0300

    немного изменил базу данных

---
+++ lesson-6/v/v_admin.php
@@ -0,0 +1,17 @@
+<main>
+    <div class="conteiner">
+        <div class="admin">
+            <h1>Выберете что редактировать!</h1>
+            <div>
+                <button onclick="adminGoods()">Товары</button>
+                <button onclick="adminUser()">Пользователей</button>
+            </div>
+        </div>
+    </div>
+    <button
+            class="add_goods"
+            onclick="addGoods()"
+    >
+        + add
+    </button>
+</main>
